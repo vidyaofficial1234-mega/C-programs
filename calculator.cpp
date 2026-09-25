dc306eca21e45558a0e0cb6bdd92dadebbb40828
Vidya <vidya.mega1234@gmail.com>
2026-09-25 17:16:22 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cout<<"Enter two Numbers: ";
+  cin>>a>>b;
+  cout<<"Sum="<<a+b<<endl;
+  cout<<"Difference="<<a-b<<endl;
+  cout<<"Product="<<a*b<<endl;
+
+  return 0;
+}
